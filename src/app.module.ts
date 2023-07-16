97d11d9be7fe142abdcc64661344ea74f8b57e47
bensonnn7 <38306742+bensonnn7@users.noreply.github.com>
2023-07-16 16:58:54 -0700

    init user entity, connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'fortune-cookie',
+      entities: [User],
+      // entities: ['dist/**/*.entity{.ts,.js}'], // Path to your entity files
+      synchronize: true,
+      // poolSize: 10,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  // @Column()
+  // createdAt: Date;
+
+  // @Column()
+  // updatedAt: Date;
+}
